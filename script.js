d42c734f90ead875dc1d1662f5536c951eaab6a1
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-06-28 19:22:39 -0700

    started Js file, completely failed, now restarting from scratch

---
+++ script.js
@@ -0,0 +1,5 @@
+var includeNums = document.getElementById("includeNumbers");
+var includeSyms = document.getElementById("includeSymbols");
+var includeLowercase = document.getElementById("includeLowercase");
+var includeUppercase = document.getElementById("includeUppercase");
+
